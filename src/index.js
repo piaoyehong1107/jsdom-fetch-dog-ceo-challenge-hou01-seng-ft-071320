17d29f15c985716acefdbf74d2d1e2cb70a0f9fe
Yehong Piao <piaoyehong1107@gmail.com>
2020-08-31 23:02:42 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -51,7 +51,7 @@ function changecolor(e){
     e.target.style.color = 'blue' 
 }
 
-document.getElementById('breed-dropdown',(e) => selectDog(e, allbreeds))
+document.getElementById('breed-dropdown').addEventListener("click", (e) => selectDog(e, allbreeds))
 
 
 document.addEventListener('DOMContentLoaded', function() {
