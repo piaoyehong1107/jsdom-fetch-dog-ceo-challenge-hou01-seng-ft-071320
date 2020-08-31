bbf4e2dbf3a8ee3b30f0bc661168ab6998b1bfea
Yehong Piao <piaoyehong1107@gmail.com>
2020-08-31 22:35:12 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,1 +1,49 @@
 console.log('%c HI', 'color: firebrick')
+let allbreeds=[];
+function logimg(){
+  fetch("https://dog.ceo/api/breeds/image/random/4")
+.then(res=>res.json())
+.then(json=>displayimg(json))
+function displayimg(dogs){
+  console.log(dogs)
+  // const list = document.querySelector('#dog-image-container')
+  // dogs.message.forEach(dog=>{
+  //   h=document.createElement("img")
+  //   h.src=dog
+  //   list.appendChild(h)
+  // })
+}
+}
+
+function fatchbreed(){
+  fetch('https://dog.ceo/api/breeds/list/all')
+      .then(res=>res.json())
+      .then(breeds=>displaybreed(breeds))
+}
+
+function displaybreed(breeds){
+        const list = document.getElementById('dog-breeds')
+      //breeds = Object.keys(breeds.message);
+        for (const key in breeds.message){
+            allbreeds.push(key)
+            h=document.createElement("li")
+            h.innerText=key
+            list.appendChild(h);
+            h.addEventListener('click', changecolor);
+            options=document.querySelector("select")
+            options.addEventListener('change',selectDog)
+        }
+}
+function selectDog(e){
+ console.log(allbreeds.filter((breed)=>{ console.log(breed[0])
+}
+function changecolor(e){
+    e.target.style.color = 'blue' 
+}
+
+
+
+document.addEventListener('DOMContentLoaded', function() {
+ logimg();
+ fatchbreed();
+})
