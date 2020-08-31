b8b520929ff0568127f37f6aefae635365f3b8fb
Yehong Piao <piaoyehong1107@gmail.com>
2020-08-31 23:14:51 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -28,14 +28,12 @@ function displaybreed(breeds){
   console.log({breeds})
   const list = document.getElementById('dog-breeds')
   breeds.forEach(key => {
-      allbreeds.push(key)
+      // allbreeds.push(key)
       h=document.createElement("li")
       h.innerText=key
       list.appendChild(h);
       h.addEventListener('click', changecolor);
   })
-  console.log({allbreeds})
-  
 }
 
 function selectDog(e){
